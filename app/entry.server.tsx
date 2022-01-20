c7386fc08b3f3e6b9a71dbab608e3103d160185b
kiliman <kiliman@gmail.com>
2022-01-20 18:17:44 -0500

    🔨 Add check for /blog/test to render as JSON to test etag support

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,30 @@
-import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
-import type { EntryContext } from "remix";
+import { renderToString } from 'react-dom/server'
+import { RemixServer, useBeforeUnload } from 'remix'
+import type { EntryContext } from 'remix'
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
 ) {
   let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+    <RemixServer context={remixContext} url={request.url} />,
+  )
 
-  responseHeaders.set("Content-Type", "text/html");
+  const url = new URL(request.url)
+  if (url.pathname === '/blog/test') {
+    responseHeaders.set('Content-Type', 'application/json')
 
-  return new Response("<!DOCTYPE html>" + markup, {
+    return new Response(JSON.stringify(remixContext.routeData), {
+      status: responseStatusCode,
+      headers: responseHeaders,
+    })
+  }
+  responseHeaders.set('Content-Type', 'text/html')
+
+  return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
-    headers: responseHeaders
-  });
+    headers: responseHeaders,
+  })
 }
