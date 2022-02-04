44fa7a3edb3f1cbb1d1a24645673595ace8a9b54
kiliman <kiliman@gmail.com>
2022-02-04 10:53:06 -0500

    🔨 Update deployment script to post current commit sha for etag
    Right now ETag is generated from content hash, but resulting HTML could change depending on code, so also include the commit SHA in ETag.

---
+++ app/routes/api/update-deploy-sha.ts
@@ -0,0 +1,18 @@
+import { ActionFunction, json } from 'remix'
+declare var CONTENT: KVNamespace
+declare var POST_API_KEY: string
+
+export const action: ActionFunction = async ({ request }) => {
+  try {
+    const key = request.headers.get('Authorization')
+    if (key !== `Bearer ${POST_API_KEY}`) {
+      return new Response(`Unauthorized ${key}`, { status: 401 })
+    }
+    const data = await request.json()
+    await CONTENT.put('$$deploy-sha', JSON.stringify(data))
+    return json({ success: true })
+  } catch (e) {
+    //@ts-expect-error
+    return json({ message: e.message, stack: e.stack })
+  }
+}
