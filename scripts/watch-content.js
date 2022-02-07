5b03242b1436b0a1c115b9e2177b1762ceb467b8
kiliman <kiliman@gmail.com>
2022-02-07 15:25:54 -0500

    🔨 Update content-watcher

--- scripts/watch-content.js
+++ scripts/watch-content.js
@@ -1,9 +1,11 @@
 const chokidar = require('chokidar')
 const fs = require('fs')
+const path = require('path')
 const exec = require('util').promisify(require('child_process').exec)
-
 const cacheFilePath = './content/.cache.json'
-const force = true
+const refreshFilePath = './app/.refresh.ignore'
+let refreshTimeout = undefined
+let force = true
 
 ;(async function () {
   await main()
@@ -15,54 +17,45 @@ async function main() {
   if (fs.existsSync(cacheFilePath)) {
     cache = JSON.parse(fs.readFileSync(cacheFilePath))
   }
+
   try {
     chokidar.watch('./content').on('all', async (event, path) => {
+      console.log(event, path)
       if (event === 'addDir') return
-
+      const filePath = path
       const { match, dir, file } = validContentPath(path)
       if (!match) return
 
       console.log({ event, path, dir, file })
-      const lastModified = fs.statSync(path).mtimeMs
-      // check for changes
-      if (!force && cache[path] && cache[path] === lastModified) {
+      const lastModified = Math.floor(fs.statSync(path).mtimeMs)
+      if (!force && cache[path] && cache[path].lastModified === lastModified) {
         console.log(`${path} has not changed`)
         return
       }
 
-      if (file === '_series.mdx') {
-        const { frontmatter, filelist } = await parseSeries(path)
-        console.log({ frontmatter, filelist })
-        updateCache(cache, dir, {
-          type: 'series',
-          frontmatter,
-          filelist,
-          lastModified,
-        })
-        return
+      // reset existing timer so we don't build multiple times
+      if (refreshTimeout) {
+        clearTimeout(refreshTimeout)
+        refreshTimeout = undefined
       }
 
-      const parts = dir.split('/')
-      let series = undefined
-      if (parts.length >= 3) {
-        series = parts.slice(0, 3).join('/')
-        if (cache[series]?.type === 'series') {
-          console.log(`Part of series ${series}`)
-        }
-        if (file === 'index.mdx') {
-          path = dir // just compile the directory
-        }
-      }
-
-      console.log(`compling ${path}`)
+      console.error('Compiling', path)
       const results = await doCompile(path)
+      console.error('results', results[path])
+      if (!results[path]) return
+
       const { hash } = results[path]
       console.log(results)
-      updateCache(cache, path, {
-        series,
+      updateCache(cache, filePath, {
+        //series,
         lastModified,
         hash,
       })
+      refreshTimeout = setTimeout(() => {
+        // update refresh file to trigger rebuild/refresh
+        fs.writeFileSync(refreshFilePath, String(lastModified), 'utf8')
+        refreshTimeout = undefined
+      }, 1000)
     })
   } catch (e) {
     console.error(e)
@@ -75,8 +68,7 @@ function updateCache(cache, path, entry) {
 }
 
 async function doCompile(path) {
-  console.log(`🛠 Compiling ${path}...`)
-  const command = `cd scripts/mdx && node compile-mdx.mjs --root ../.. --json --file ${path}`
+  const command = `node scripts/mdx/compile-mdx.mjs --json --file ${path}`
   let out = await exec(command).catch(e => {
     console.error(e)
   })
@@ -90,41 +82,4 @@ function validContentPath(contentPath) {
   if (!match) return { match: false }
   const { dir, file } = match.groups
   return { match: true, dir, file }
-}
-
-function parseSeries(path) {
-  const file = fs.readFileSync(path, 'utf8')
-  const lines = file.split('\n')
-  const frontmatter = {}
-  const filelist = []
-  let state = 'START'
-  for (let line of lines) {
-    line = line.trim()
-    if (!line || line.startsWith('#')) continue
-    if (state === 'START') {
-      if (line.startsWith('---')) {
-        state = 'FRONTMATTER'
-      }
-    } else if (state === 'FRONTMATTER') {
-      if (line.startsWith('---')) {
-        state = 'CONTENT'
-      } else {
-        const { name, value } = parseLine(line)
-        frontmatter[name] = value
-      }
-    } else if (state === 'CONTENT') {
-      if (line.startsWith('---')) {
-        state = 'END'
-      }
-      filelist.push(line)
-    }
-  }
-  return { frontmatter, filelist }
-}
-
-function parseLine(line) {
-  const parts = line.split(':')
-  const name = parts[0].trim()
-  const value = parts[1].trim()
-  return { name, value }
 }
