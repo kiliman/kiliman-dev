13e12a5342070f0fb7f71c639b50ba8dee09c812
kiliman <kiliman@gmail.com>
2022-02-07 15:25:44 -0500

    🔨 Update mdx compiler

--- scripts/mdx/compile-mdx.mjs
+++ scripts/mdx/compile-mdx.mjs
@@ -15,10 +15,7 @@ import { Command } from 'commander/esm.mjs'
   config()
   const program = new Command()
   program
-    .requiredOption(
-      '-R, --root <path>',
-      'Root path (content is relative to root',
-    )
+    .option('-R, --root <path>', 'Root path (content is relative to root')
     .option('-f, --file [files...]', 'Files to compile')
     .option('-j, --json', 'Output JSON')
 
@@ -29,102 +26,158 @@ import { Command } from 'commander/esm.mjs'
     process.exit(1)
   }
 
-  const rootPath = options.root
+  const rootPath = options.root ?? process.cwd()
   const mdxPaths = options.file
   const results = {}
   let hasError = false
   const processed = {}
-  const seriesList = {}
-  for (let mdxPath of mdxPaths) {
-    console.error(`Compiling ${mdxPath}...`)
-    const fullPath = path.join(rootPath, mdxPath)
-    if (processed[mdxPath]) continue
-    processed[mdxPath] = true
+  const seriesMap = new Map()
+  // process files until empty
+  while (mdxPaths.length) {
+    let mdxPath = mdxPaths[0]
+    try {
+      if (processed[mdxPath]) continue
 
-    const parts = mdxPath.split('/')
-    const slug = parts.slice(1).join('/').replace('.mdx', '')
-    let series = undefined
-    if (parts.length > 3) {
-      const seriesPath = path.join(parts.slice(0, 3).join('/'), '_series.mdx')
-      if (
-        !seriesList[seriesPath] &&
-        fs.existsSync(path.join(rootPath, seriesPath))
-      ) {
-        seriesList[seriesPath] = parseSeries(path.join(rootPath, seriesPath))
+      // remove index.mdx from path
+      if (mdxPath.endsWith('/index.mdx')) {
+        mdxPath = mdxPath.replace('/index.mdx', '')
       }
-      series = seriesList[seriesPath]
-    }
 
-    let mdxSource = ''
-    let files = {}
-    if ((await fsp.lstat(fullPath)).isDirectory()) {
-      mdxSource = await fsp.readFile(`${fullPath}/index.mdx`, 'utf8')
-      const mdxFiles = (await fsp.readdir(fullPath)).filter(
-        filename => filename !== 'index.mdx',
-      )
-      const results = await Promise.all(
-        mdxFiles.map(async filename =>
-          fsp.readFile(`${fullPath}/${filename}`, 'utf8'),
-        ),
-      )
-      files = Object.fromEntries(
-        results.map((content, i) => [`./${mdxFiles[i]}`, content]),
-      )
-    } else {
-      mdxSource = await fsp.readFile(fullPath, 'utf8')
-    }
-    const { frontmatter, code } = await bundleMDX({
-      source: mdxSource,
-      files,
-      xdmOptions(options) {
-        // options.remarkPlugins = [
-        //   ...(options.remarkPlugins ?? []),
-        //   remarkMdxCodeMeta,
-        // ]
-        options.rehypePlugins = [
-          ...(options.rehypePlugins ?? []),
-          rehypeHighlight,
-        ]
-        return options
-      },
-    })
-    const Component = getMDXComponent(code)
-    const html = renderToString(React.createElement(Component))
-    const hasComponents = Object.keys(files).length > 0
+      console.error(`Compiling ${mdxPath}...`)
+      const fullPath = path.join(rootPath, mdxPath)
 
-    const hash = crypto
-      .createHash('sha256')
-      .update(frontmatter + code)
-      .digest('hex')
+      const parts = mdxPath.split('/')
+      const slug = parts.slice(1).join('/').replace('.mdx', '')
 
-    const response = await fetch(`${process.env.API_URL}/post-content`, {
-      method: 'post',
-      body: JSON.stringify({
-        slug,
-        hash,
-        frontmatter,
-        series,
-        html,
-        code: hasComponents ? code : undefined,
-      }),
-      headers: {
-        authorization: `Bearer ${process.env.POST_API_KEY}`,
-      },
-    })
-    if (!response.ok) {
-      const body = await response.text()
+      let mdxSource = ''
+      let files = {}
+      const exists = fs.existsSync(fullPath)
+      if (exists && (await fsp.lstat(fullPath)).isDirectory()) {
+        mdxSource = await fsp.readFile(`${fullPath}/index.mdx`, 'utf8')
+        const mdxFiles = (await fsp.readdir(fullPath)).filter(
+          filename => filename !== 'index.mdx',
+        )
+        const results = await Promise.all(
+          mdxFiles.map(async filename =>
+            fsp.readFile(`${fullPath}/${filename}`, 'utf8'),
+          ),
+        )
+        files = Object.fromEntries(
+          results.map((content, i) => [`./${mdxFiles[i]}`, content]),
+        )
+      } else {
+        if (!fullPath.endsWith('.mdx')) fullPath += '.mdx'
+        // verify file exists
+        if (!fs.existsSync(fullPath)) continue
+        mdxSource = await fsp.readFile(fullPath, 'utf8')
+      }
+      const cwd = Object.keys(files).length
+        ? path.join(rootPath, mdxPath)
+        : undefined
+      console.error(`compiling ${mdxPath} with cwd ${cwd}`)
+      const { frontmatter, code } = await bundleMDX({
+        source: mdxSource,
+        files,
+        // set cwd if mdx has file imports
+        cwd,
+        xdmOptions(options) {
+          // options.remarkPlugins = [
+          //   ...(options.remarkPlugins ?? []),
+          //   remarkMdxCodeMeta,
+          // ]
+          options.rehypePlugins = [
+            ...(options.rehypePlugins ?? []),
+            rehypeHighlight,
+          ]
+          return options
+        },
+      })
+      const Component = getMDXComponent(code)
+      const html = renderToString(React.createElement(Component))
+      const hasComponents = Object.keys(files).length > 0
+
+      let seriesRoot = undefined
+      let series = undefined
+      if (parts.length > 3) {
+        // get series root (strip content/ from path)
+        seriesRoot = path.join(parts.slice(1, 3).join('/'))
+      }
+      if (parts[parts.length - 1] === 'series.mdx') {
+        // this is a series so get frontmatter
+        frontmatter.slug = slug
+        seriesMap.set(seriesRoot, frontmatter)
+        Array.from(frontmatter['posts']).forEach(post => {
+          let postPath = path.join('content', seriesRoot, post)
+          const fullPath = path.join(rootPath, postPath)
+          const exists = fs.existsSync(fullPath)
+          if (!exists) {
+            postPath += '.mdx'
+          }
+          if (!mdxPaths.includes(postPath)) {
+            mdxPaths.push(postPath)
+          }
+        })
+      } else if (seriesRoot) {
+        // this is a blog post in a series
+        series = seriesMap.get(seriesRoot)
+        if (!series) {
+          // series not in local map, so get it from the API
+          const response = await fetch(
+            `${process.env.API_URL}/get-content/${seriesRoot}/series`,
+          )
+          if (response.ok) {
+            series = await response.json()
+            series = frontmatter
+            seriesMap.set(seriesRoot, series)
+          } else {
+            // series not found, so reprocess this file after the series is created
+            continue
+          }
+        }
+      }
+
+      const hash = crypto
+        .createHash('sha256')
+        .update(
+          `${JSON.stringify(frontmatter)}${
+            JSON.stringify(series) ?? ''
+          }${code}`,
+        )
+        .digest('hex')
+
+      const response = await fetch(`${process.env.API_URL}/post-content`, {
+        method: 'post',
+        body: JSON.stringify({
+          slug,
+          hash,
+          frontmatter,
+          series,
+          html,
+          code: hasComponents ? code : undefined,
+        }),
+        headers: {
+          authorization: `Bearer ${process.env.POST_API_KEY}`,
+        },
+      })
+      if (!response.ok) {
+        const body = await response.text()
+        results[mdxPath] = {
+          status: response.status,
+          statusText: response.statusText,
+          body,
+        }
+        hasError = true
+      }
       results[mdxPath] = {
         status: response.status,
         statusText: response.statusText,
-        body,
+        slug,
+        hash,
       }
-      hasError = true
-    }
-    results[mdxPath] = {
-      status: response.status,
-      statusText: response.statusText,
-      slug,
-      hash,
+    } finally {
+      // remove file from list
+      processed[mdxPath] = true
+      mdxPaths.splice(0, 1)
     }
   }
   if (options.json) {
@@ -132,44 +185,3 @@ import { Command } from 'commander/esm.mjs'
   }
   process.exit(hasError ? 1 : 0)
 })()
-
-function parseSeries(path) {
-  const file = fs.readFileSync(path, 'utf8')
-  const slug = path.substring(
-    path.indexOf('content/') + 8,
-    path.indexOf('_series') - 1,
-  )
-  const lines = file.split('\n')
-  const frontmatter = {}
-  const filelist = []
-  let state = 'START'
-  for (let line of lines) {
-    line = line.trim()
-    if (!line || line.startsWith('#')) continue
-    if (state === 'START') {
-      if (line.startsWith('---')) {
-        state = 'FRONTMATTER'
-      }
-    } else if (state === 'FRONTMATTER') {
-      if (line.startsWith('---')) {
-        state = 'CONTENT'
-      } else {
-        const { name, value } = parseLine(line)
-        frontmatter[name] = value
-      }
-    } else if (state === 'CONTENT') {
-      if (line.startsWith('---')) {
-        state = 'END'
-      }
-      filelist.push(line)
-    }
-  }
-  return { slug, frontmatter, filelist }
-}
-
-function parseLine(line) {
-  const parts = line.split(':')
-  const name = parts[0].trim()
-  const value = parts[1].trim()
-  return { name, value }
-}
