930c8a07ae6022ed98b1f1a6479f07b516410df6
kiliman <kiliman@gmail.com>
2022-08-24 18:02:57 -0400

    🔨 Update blog index to display thumbnails

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,3 +1,4 @@
+import { CalendarIcon } from '@heroicons/react/outline'
 import {
   HeadersFunction,
   json,
@@ -5,6 +6,7 @@ import {
   MetaFunction,
 } from '@remix-run/cloudflare'
 import { Link as RemixLink, useLoaderData } from '@remix-run/react'
+import Logo from '~/components/Logo'
 import { siteTitle } from '~/utils/constants'
 declare var CONTENT: KVNamespace
 
@@ -34,20 +36,53 @@ export default function Index() {
     <div className="container flex flex-col gap-4 m-auto md:flex-row">
       <div className="basis-2/3">
         <h1 className="text-3xl font-bold">Blog Posts</h1>
-        <ul className="mt-6">
+        <ul className="mt-6 flex flex-col w-full gap-y-4 sm:gap-y-8 gap-x-4">
           {posts.map((post: any) => (
             <ListItem key={post.slug}>
-              <Link to={`/${post.slug}`}>{post.title}</Link>
-              <p className="mt-1 text-sm text-slate-300 line-clamp-2">
-                {post.description}
-              </p>
-              {post.series && (
-                <p>
-                  Series{' '}
-                  <Link to={`/${post.series.slug}`}>{post.series.title}</Link>{' '}
-                  (Post #{post.series.sequence} of {post.series.count})
-                </p>
-              )}
+              <div className="flex">
+                <Link to={`/${post.slug}`}>
+                  {post.image ? (
+                    <img
+                      className="min-w-[64px] min-h-[64px] h-16 w-16 rounded mr-4"
+                      src={post.image.url.replace(
+                        /public$/,
+                        'width=256,height=256,fit=crop,gravity=0.5x0.5',
+                      )}
+                      alt={post.title}
+                    />
+                  ) : (
+                    <Logo className="min-w-[64px] min-h-[64px] h-16 w-16 rounded mr-4" />
+                  )}
+                </Link>
+                <div>
+                  <Link to={`/${post.slug}`}>
+                    <div>{post.title}</div>
+                  </Link>
+                  <div className="flex items-center gap-2">
+                    <CalendarIcon className="w-4 h-4 text-slate-200" />
+                    <div className="text-sm text-slate-200">
+                      {post.published && post.published !== 'draft'
+                        ? new Intl.DateTimeFormat('en-us', {
+                            timeZone: 'UTC',
+                            dateStyle: 'long',
+                          }).format(new Date(post.published))
+                        : 'Draft'}
+                    </div>
+                  </div>
+                  <p className="mt-1 text-sm text-slate-300 line-clamp-2">
+                    {post.description}
+                  </p>
+                  {/* {post.series && (
+                    <p className="mt-2">
+                      Series{' '}
+                      <Link to={`/${post.series.slug}`}>
+                        {post.series.title}
+                      </Link>{' '}
+                      (Post #{post.series.sequence} of {post.series.count})
+                    </p>
+                  )} */}
+                </div>
+              </div>
             </ListItem>
           ))}
         </ul>
@@ -57,10 +92,29 @@ export default function Index() {
         <ul className="mt-6">
           {series.map(({ slug, ...frontmatter }: any) => (
             <ListItem key={slug}>
-              <Link to={`/${slug}`}>{frontmatter.title}</Link>
-              <p className="mt-1 text-sm text-slate-300 line-clamp-2">
-                {frontmatter.description}
-              </p>
+              <Link to={`/${slug}`}>
+                <div className="flex">
+                  {frontmatter.image ? (
+                    <img
+                      className="min-w-[64px] min-h-[64px] h-16 w-16 rounded mr-4"
+                      src={frontmatter.image?.url.replace(
+                        /public$/,
+                        'width=256,height=256,fit=crop,gravity=0.5x0.5',
+                      )}
+                      alt={frontmatter.title}
+                    />
+                  ) : (
+                    <Logo className="min-w-[64px] min-h-[64px] h-16 w-16 rounded mr-4" />
+                  )}
+
+                  <div>
+                    <div>{frontmatter.title}</div>
+                    <p className="mt-1 text-sm text-slate-300 line-clamp-2">
+                      {frontmatter.description}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </ListItem>
           ))}
         </ul>
