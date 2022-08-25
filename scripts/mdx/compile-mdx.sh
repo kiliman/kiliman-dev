a59f1d3655fb6e6617f9922f020bcb23d3f9d772
kiliman <kiliman@gmail.com>
2022-08-25 09:55:05 -0400

    🔨 Fix API token

--- scripts/mdx/compile-mdx.sh
+++ scripts/mdx/compile-mdx.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$(pwd)
 pushd "$SCRIPT_DIR" >/dev/null 2>&1
-node -r esbuild-runner/register compile-mdx.ts --api https://kiliman.dev/api --key 6d92a1f4-a34a-43c8-9a61-138431f67af4 --root "$ROOT_DIR" --json --file $*
+node -r esbuild-runner/register compile-mdx.ts --root "$ROOT_DIR" --json --file $*
 popd >/dev/null 2>&1
